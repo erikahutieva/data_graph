ebd9966dfcd13f481144ca4c159186bda4ee5b84
Erika <erika@MacBook-Pro-Erika.local>
2025-03-15 23:09:44 +0300

    4 izm and clast

--- OOP/car-4-izm.py
+++ OOP/car-4-izm.py
@@ -12,54 +12,79 @@ with open("CarPrice_Assignment.csv", "r", encoding="utf-8") as file:
             row = dict(zip(head, values))
             data.append(row)
 
-
-columns = ["horsepower", "enginesize",  "curbweight","price"]
+columns = ["horsepower", "enginesize", "curbweight", "price"]
 data_special = []
 for row in data:
     data_special.append([float(row[col]) for col in columns])
 array = np.array(data_special)
-
-
-weights = np.array([0.9, 7.4, 9.0, 0.1])
+weights = np.array([0.9, 7.4, 9.0, 0.01])
 standard = array * weights
-
-
 x = standard[:, 0]  
 y = standard[:, 1]  
-z = standard[:, 2] 
+z = standard[:, 2]   
+sizes = standard[:, 3] 
 
-sizes = standard[:, 3]/10
+
+def get_cluster(x, y, a, b):
+    labels = []
+    for x_val, y_val in zip(x, y):
+        func = a * x_val + b
+        if y_val > func:
+            labels.append(1)
+        else:
+            labels.append(0)
+    return np.array(labels)
+
+a, b,c, d, e, f = 9, 100, 150, 7500,16, 9000
+xy = get_cluster(x, y, a, b)
+#print(xy)
+xz = get_cluster(x, z, c, d)
+#print(xz)
+yz = get_cluster(y, z, e, f)  
+#print(yz)
+colors = {0: 'blue', 1: 'orange', 2: 'green', 3: 'red', 4: 'purple', 5: 'brown'}
+
+
 
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x, y, z, s=sizes, c='blue', alpha=0.5)
+ax3d = fig.add_subplot(1, 1, 1, projection='3d')
+ax3d.scatter(x, y, z, s=sizes, c='red', alpha=0.5)
+ax3d.set_xlabel("Мощность")
+ax3d.set_ylabel("Объём двигателя")
+ax3d.set_zlabel("Масса")
+ax3d.set_title("график (цена 4 измер)")
 
-ax.set_xlabel("Мощность")
-ax.set_ylabel("Объём двигателя")
-ax.set_zlabel("Масса")
-ax.set_title("график (Цена - 4-я координата)")
 
 
-fig, proect = plt.subplots(1, 3)
 
-proect[0].scatter(x, y, s=sizes, c='blue', alpha=0.5)
-proect[0].set_title("Мощность и Объём двигателя")
 
-proect[0].set_xlim([50, 150])
-proect[0].set_ylim([500, 1500])
+fig2, axs = plt.subplots(1, 3)
+for clust in [0, 1]:
+    idx = (xy == clust)  # фильтр по кластеру
+    axs[0].scatter(x[idx], y[idx], s=sizes[idx], c=colors[clust], alpha=0.5)
+axs[0].set_title("Мощность vs Объём двигателя")
 
-proect[1].scatter(x, z, s=sizes, c='green', alpha=0.5)
-proect[1].set_title("Мощность и Масса")
 
-proect[1].set_xlim([50, 150])
-proect[1].set_ylim([15000, 30000])
 
-proect[2].scatter(y, z, s=sizes, c='red', alpha=0.5)
-proect[2].set_title("Объём двигателя и Масса")
-proect[2].set_xlim([500, 1500])
-proect[1].set_ylim([17000, 33000])
 
+
+
+for clust in [2, 3]:
+    idx = (xz == (clust - 2)) 
+    axs[1].scatter(x[idx], z[idx], s=sizes[idx], c=colors[clust], alpha=0.5)
+axs[1].set_title("Мощность vs Масса")
+
+
+
+
+
+for clust in [4, 5]:
+    idx = (yz == (clust - 4)) 
+    axs[2].scatter(y[idx], z[idx], s=sizes[idx], c=colors[clust], alpha=0.5)
+axs[2].set_title("Объём двигателя vs Масса")
+
+print(sizes)
 
 plt.tight_layout()
 plt.show()
